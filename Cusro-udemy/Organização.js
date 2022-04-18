64bc440f4b25f49ec4c14a0e89a40c2fa1333a9e
Matheus Rocha <69425699+matheus-coder@users.noreply.github.com>
2022-04-17 22:40:12 -0300

    Curso - Udemy-WEB

---
+++ Cusro-udemy/Organização.js
@@ -0,0 +1,9 @@
+console.log("<< Sentença de codigo")
+
+/*JS é organizado em (sentença de codigo),
+*podem ounão termnmar com ";"
+*tambem é organizadoem chaves e em (bloco e codigos)
+*/
+
+//comentari de linha
+// aula: 39 tipagem forte https://www.udemy.com/course/curso-web/learn/lecture/8778012#overview
